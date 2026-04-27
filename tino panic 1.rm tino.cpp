480374a954018327eeebeedf3c09d3d93494ab29
pinotanic123 <tinopanic321@gmail.com>
2026-04-27 10:56:52 +0200

    Add files via upload

---
+++ tino panic 1.rm tino.cpp
@@ -0,0 +1,39 @@
+﻿// tino panic
+
+
+#include <stack>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    stack<char> p, pp;
+    string s;
+
+    cout << "Upisi neka slova: ";
+    cin >> s;
+
+    
+    for (char c : s) {
+        if (!p.empty() && p.top() == c) {
+            p.pop();   
+        }
+        else {
+            p.push(c);
+        }
+    }
+
+     
+    while (!p.empty()) {
+        pp.push(p.top());
+        p.pop();
+    }
+
+    while (!pp.empty()) {
+        cout << pp.top();
+        pp.pop();
+    }
+
+
+    return 0;
+}
